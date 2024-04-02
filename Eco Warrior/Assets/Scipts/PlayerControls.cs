3f95825aa3ed5f38d39681c1cd61d5706b855cbd
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-04-02 15:23:31 -0500

    Added input system package

--- Eco Warrior/Assets/Scipts/PlayerControls.cs
+++ Eco Warrior/Assets/Scipts/PlayerControls.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
