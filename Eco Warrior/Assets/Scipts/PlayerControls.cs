0de908197742659abc60728ad9f854324fcfd9be
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-04-02 16:19:56 -0500

    Update PlayerControls.cs

--- Eco Warrior/Assets/Scipts/PlayerControls.cs
+++ Eco Warrior/Assets/Scipts/PlayerControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+    private PlayerControls PlayerControls;
+    private Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
