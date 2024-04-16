a9126324de6e420b61ef6f0ee79f2be0fa52ce92
Mateo0G <mateo.ghercioiu@gmail.com>
2024-04-16 15:06:09 -0500

    Max Health and Max XP values

---
+++ Eco Warrior/Assets/Progression System/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    int currentHealth, maxHealth,
+        currentExperience, maxExperience,
+        currentLevel;
+}
