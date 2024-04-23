1e290babe7978a07b3c5897690be638029606dff
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-04-23 17:00:16 -0500

    Revert "push enemy ai movement"
    This reverts commit 3d6281709a1dfe4675883ba07fecfe19070fc412.

--- Eco Warrior/Assets/FollowPlayer.cs
+++ Eco Warrior/Assets/FollowPlayer.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-
-    private bool checkTrigger;
-    public float speed;
-    public Transform target;
-
+    // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (checkTrigger)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-
-        if (other.name == "Player")
-        {
-            checkTrigger = true;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.name == "Player")
-        {
-            checkTrigger = false;
-        }
+        
     }
 }
