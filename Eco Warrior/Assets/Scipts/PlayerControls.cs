01f79de172b7192e67f4947fc93805f0694ea010
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-04-23 17:28:01 -0500

    Brought the new PlayerControls.cs

--- Eco Warrior/Assets/Scipts/PlayerControls.cs
+++ Eco Warrior/Assets/Scipts/PlayerControls.cs
@@ -8,9 +8,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+    private GameObject sword;
     private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        sword = GameObject.Find("Sword");
     }
     private void OnEnable() {
         playerControls.Enable();
@@ -27,15 +33,32 @@ public class PlayerController : MonoBehaviour
         PlayerInput();
     }
     private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
         Move();
     }
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
-        
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+    private void AdjustPlayerFacingDirection()
+    {
+        if (movement.x != 0)
+        {
+            if (movement.x < 0)
+            {
+                //mySpriteRenderer.flipX = ;
+            }
+            //.87;
+            //-.82;
+            
+            //sword.GetComponent<SpriteRenderer>().flipX = mySpriteRenderer.flipX;
+            //sword.GetComponent<Transform>().position.x -= 1.69;
+        }
     }
 }
